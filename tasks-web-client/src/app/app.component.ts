707396ffc10daf8d67370b991927fa7beb97ed5e
Roberto Clapis <empijei@users.noreply.github.com>
2021-03-13 09:27:22 +0100

    Added some TODO-list boilerplate.

--- tasks-web-client/src/app/app.component.ts
+++ tasks-web-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasks-web-client';
+
+  todo = [
+    'Find a name for the app',
+    'Create UI mocks',
+    'Understand why theming is not working',
+  ];
+
+  done = [
+    'Create boilerplate',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
--- tasks-web-client/src/app/app.module.ts
+++ tasks-web-client/src/app/app.module.ts
@@ -1,18 +1,40 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// CDK
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+// Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+// Local imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    // CDK
+    DragDropModule,
+
+    // Material
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
